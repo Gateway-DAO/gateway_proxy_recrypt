62a8fec6d0bd03b0aad21de911ff122f5d0d8778
msgateway <166838722+msgateway@users.noreply.github.com>
2024-09-06 12:35:07 -0400

    added log statements
    added log statements

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+use log::{info, debug, warn, error};
 pub use recrypt::{
     api::{Plaintext, PrivateKey, PublicKey, SigningKeypair, EncryptedValue, TransformKey},
     prelude::*,
 };
 pub use bincode;
 pub use recrypt::api::DefaultRng;
+use solana_sdk::signature::{Keypair, Signer};
+
 
 // Define chunk_size as a constant
 const CHUNK_SIZE: usize = 384; // You can set this to any appropriate value
@@ -15,11 +18,14 @@ pub fn generate_transform_key(
     target_pub_key: &PublicKey,
     signing_keypair: &SigningKeypair,
 ) -> Result<TransformKey, Box<dyn std::error::Error>> {
+    info!("Generating transform key...");
     let transform_key = recrypt.generate_transform_key(
         initial_priv_key,
         target_pub_key,
         signing_keypair,
     )?;
+    debug!("Transform Key: {:?}", transform_key);
+    debug!("Transform key generated successfully.");
     Ok(transform_key)
 }
 
@@ -30,11 +36,13 @@ pub fn transform_encrypted_value(
     transform_key: TransformKey,
     signing_keypair: &SigningKeypair,
 ) -> Result<EncryptedValue, Box<dyn std::error::Error>> {
+    info!("Transforming encrypted value...");
     let transformed_value = recrypt.transform(
         encrypted_value,
         transform_key,
         signing_keypair,
     )?;
+    debug!("Encrypted value transformed successfully.");
     Ok(transformed_value)
 }
 
@@ -48,55 +56,75 @@ pub fn get_symmetric_key(input: &str) -> String {
 }
 
 pub fn get_serialized_symmetric_key(input: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    info!("Serializing symmetric key...");
     let symm_key = get_symmetric_key(input);
     let serialized_symm_key = bincode::serialize(&symm_key)?;
+    debug!("Symmetric key serialized successfully.");
     Ok(serialized_symm_key)
 }
 
 pub fn generate_asymmetric_key(recrypt: &mut Recrypt<Sha256, Ed25519, RandomBytes<DefaultRng>>) -> Result<(PrivateKey, PublicKey), Box<dyn std::error::Error>> {
+    info!("Generating asymmetric key pair...");
     let (priv_key, pub_key) = recrypt.generate_key_pair()?;
+    debug!("Asymmetric Public Key: {:?}", pub_key);
+    debug!("Asymmetric key pair generated successfully.");
     Ok((priv_key, pub_key))
 }
 
 pub fn generate_signing_key(recrypt: &mut Recrypt<Sha256, Ed25519, RandomBytes<DefaultRng>>) -> SigningKeypair {
-    recrypt.generate_ed25519_key_pair()
+    info!("Generating signing keypair...");
+    let keypair = recrypt.generate_ed25519_key_pair();
+    debug!("Signing Public Key: {:?}", keypair.public_key());
+    debug!("Signing keypair generated successfully.");
+    keypair
 }
 
 pub fn split_and_pad_serialized_data(serialized_data: &[u8]) -> Vec<Plaintext> {
-    serialized_data
+    info!("Splitting and padding serialized data...");
+    let chunks: Vec<Plaintext> = serialized_data
         .chunks(CHUNK_SIZE)
         .map(|chunk| {
             let padded_chunk = pad_vec(chunk.to_vec(), CHUNK_SIZE, 0);
             Plaintext::new_from_slice(&padded_chunk).unwrap()
         })
-        .collect()
+        .collect();
+    debug!("Data split and padded into {} chunks.", chunks.len());
+    chunks
 }
 
 pub fn encrypt_chunk(recrypt: &Recrypt<Sha256, Ed25519, RandomBytes<DefaultRng>>, plaintext: &Plaintext, public_key: &PublicKey, signing_keypair: &SigningKeypair) -> Result<EncryptedValue, Box<dyn std::error::Error>> {
+    info!("Encrypting chunk...");
     let encrypted_val = recrypt.encrypt(plaintext, public_key, signing_keypair)?;
+    debug!("Chunk encrypted successfully.");
     Ok(encrypted_val)
 }
 
 pub fn decrypt_chunk(recrypt: &Recrypt<Sha256, Ed25519, RandomBytes<DefaultRng>>, encrypted_val: EncryptedValue, private_key: &PrivateKey) -> Result<Plaintext, Box<dyn std::error::Error>> {
+    info!("Decrypting chunk...");
     let decrypted_val = recrypt.decrypt(encrypted_val, private_key)?;
+    debug!("Chunk decrypted successfully.");
     Ok(decrypted_val)
 }
 
 pub fn encrypt_all_chunks(recrypt: &Recrypt<Sha256, Ed25519, RandomBytes<DefaultRng>>, chunks: &[Plaintext], public_key: &PublicKey, signing_keypair: &SigningKeypair) -> Result<Vec<EncryptedValue>, Box<dyn std::error::Error>> {
+    info!("Encrypting all chunks...");
     let mut encrypted_chunks = Vec::new();
     for pt in chunks {
         let encrypted_val = encrypt_chunk(recrypt, pt, public_key, signing_keypair)?;
         encrypted_chunks.push(encrypted_val);
     }
+    debug!("All chunks encrypted successfully.");
     Ok(encrypted_chunks)
 }
 
 pub fn decrypt_all_chunks(recrypt: &Recrypt<Sha256, Ed25519, RandomBytes<DefaultRng>>, encrypted_chunks: &[EncryptedValue], private_key: &PrivateKey) -> Result<Vec<Plaintext>, Box<dyn std::error::Error>> {
+    info!("Decrypting all chunks...");
     let mut decrypted_chunks = Vec::new();
     for encrypted_val in encrypted_chunks {
         let decrypted_val = decrypt_chunk(recrypt, encrypted_val.clone(), private_key)?;
         decrypted_chunks.push(decrypted_val);
     }
+    debug!("All chunks decrypted successfully.");
     Ok(decrypted_chunks)
 }
 
@@ -106,6 +134,7 @@ pub fn encrypt_all_chunks_as_one(
     public_key: &PublicKey,
     signing_keypair: &SigningKeypair,
 ) -> Result<EncryptedValue, Box<dyn std::error::Error>> {
+    info!("Encrypting all chunks as one...");
     let mut combined_data = Vec::new();
     for pt in chunks {
         combined_data.extend_from_slice(pt.bytes());
@@ -114,6 +143,7 @@ pub fn encrypt_all_chunks_as_one(
     let combined_plaintext = Plaintext::new_from_slice(&combined_data)?;
 
     let encrypted_val = recrypt.encrypt(&combined_plaintext, public_key, signing_keypair)?;
+    debug!("All chunks encrypted as one successfully.");
     Ok(encrypted_val)
 }
 
@@ -122,16 +152,38 @@ pub fn decrypt_all_chunks_as_one(
     encrypted_value: EncryptedValue,
     private_key: &PrivateKey,
 ) -> Result<Plaintext, Box<dyn std::error::Error>> {
+    info!("Decrypting all chunks as one...");
     let decrypted_plaintext = recrypt.decrypt(encrypted_value, private_key)?;
+    debug!("All chunks decrypted as one successfully.");
     Ok(decrypted_plaintext)
 }
 
 pub fn verify_decryption(decrypted_val: &Plaintext, expected_key: &str) -> Result<(), Box<dyn std::error::Error>> {
+    info!("Verifying decryption...");
     let decrypted_bytes = decrypted_val.bytes();
     let deserialized_str: String = bincode::deserialize(decrypted_bytes)?;
-    // Print the parameters before the assertion
-    println!("Decrypted String: {}", deserialized_str);
-    println!("Expected Key: {}", expected_key);
+    debug!("Decrypted String: {}", deserialized_str);
+    debug!("Expected Key: {}", expected_key);
     assert_eq!(expected_key, deserialized_str);
+    info!("Decryption verified successfully.");
     Ok(())
+}
+
+/// Generates a new Solana key pair.
+///
+/// # Returns
+/// A tuple containing:
+/// - `String`: The public key as a base58 encoded string.
+/// - `Vec<u8>`: The secret key as a byte array.
+pub fn generate_solana_keypair() -> (String, Vec<u8>) {
+    // Generate a new keypair
+    let keypair = Keypair::new();
+
+    // Get the public key as a base58 string
+    let public_key = keypair.pubkey().to_string();
+
+    // Get the secret key as a byte array
+    let secret_key = keypair.to_bytes().to_vec();
+
+    (public_key, secret_key)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 use std::time::Instant;
 use gw_proxy_recrypt::*;
+use log::{debug, error, info, warn};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let start = Instant::now();
+    info!("Program started for PRE.");
 
     let mut recrypt = Recrypt::new();
+    debug!("Recrypt object created.");
 
     let signing_keypair = generate_signing_key(&mut recrypt);
+    debug!("Signing keypair generated.");
 
     let (initial_priv_key, initial_pub_key) = generate_asymmetric_key(&mut recrypt)?;
+    debug!("Initial asymmetric key pair generated.");
 
     let input = "This is symmtery key";
+    info!("Input symmetric key: {}", input);
 
     let serialized_symm_keys = get_serialized_symmetric_key(input)?;
+    debug!("Serialized symmetric key obtained.");
 
     let symm_key_chunks = split_and_pad_serialized_data(&serialized_symm_keys);
+    debug!("Symmetric key chunks split and padded.");
 
     let encrypted_value = encrypt_all_chunks_as_one(&recrypt, &symm_key_chunks, &initial_pub_key, &signing_keypair)?;
+    info!("Symmetric key chunks encrypted.");
 
     let (target_priv_key, target_pub_key) = generate_asymmetric_key(&mut recrypt)?;
+    debug!("Target asymmetric key pair generated.");
 
     // Generate a transform key
     let initial_to_target_transform_key = generate_transform_key(
@@ -27,6 +38,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &target_pub_key,
         &signing_keypair,
     )?;
+    info!("Transform key generated from initial to target key.");
 
     // Transform the encrypted value
     let transformed_val = transform_encrypted_value(
@@ -35,12 +47,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         initial_to_target_transform_key,
         &signing_keypair,
     )?;
+    info!("Encrypted value transformed.");
 
     let decrypted_val = decrypt_all_chunks_as_one(&recrypt, transformed_val, &target_priv_key)?;
+    info!("Transformed value decrypted.");
 
-    verify_decryption(&decrypted_val,input)?;
+    verify_decryption(&decrypted_val, input)?;
+    info!("Decryption verified successfully.");
 
-    println!("All chunks successfully encrypted and decrypted in {:?}", start.elapsed());
+  //  println!("All chunks successfully encrypted and decrypted in {:?}", start.elapsed());
+    info!("Program completed successfully in {:?}", start.elapsed());
 
     Ok(())
 }
